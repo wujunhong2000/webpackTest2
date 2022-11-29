77e68391ca1606a366cfd071a3b4c273b0c21d22
wjh <645537705@qq.com>
2022-11-30 00:07:32 +0800

    tree-sharking-2

--- treeShakingDemo/src/app.js
+++ treeShakingDemo/src/app.js
@@ -15,6 +15,7 @@
 
 // 有副作用的文件不会进行tree-shaking，如全局样式、设置了全局配置的js文件
 // webpack4默认将所有文件视为有副作用的文件，webpack5默认做tree-shaking
-// package.js文件中配置sideEfects: true, false(所有文件都没有副作用，可以删除)), Array
+// package.js文件中配置sideEfects: true, false(所有文件都没有副作用，可以删除)), Array(["*.css"], 所有css文件都不处理)
 
 import './style.css'
+import './m.global'
