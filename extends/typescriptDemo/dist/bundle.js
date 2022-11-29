d7940a84ed8f9b514f0bd4e188f71e283720e275
wujunhong <wb.wujunhong01@mesg.corp.netease.com>
2022-11-29 16:32:16 +0800

    typescript

---
+++ extends/typescriptDemo/dist/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.ts ***!
+  \********************/
+
+// npx tsc --init  创建ts配置文件
+const age = 1;
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFhO0FBQ2I7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3R5cGVzY3JpcHREZW1vLy4vc3JjL2FwcC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcclxuLy8gbnB4IHRzYyAtLWluaXQgIOWIm+W7unRz6YWN572u5paH5Lu2XHJcbmNvbnN0IGFnZSA9IDE7XHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ extends/typescriptDemo/src/app.ts
@@ -0,0 +1,6 @@
+// npx tsc --init  创建ts配置文件
+
+// ts文件内，第三方库的引入需要配置类型模块
+// 类型模块查找地址 https://www.typescriptlang.org/dt/search
+import _ from 'lodash';
+const age: number = 1;
